01d48fa9ae9d04a80a5ae3b41ffe31263eab03de
mouaammou <mouadamassine@gmail.com>
2024-04-22 21:21:34 +0100

    the blog details looks better

--- my-first-react-app/src/BlogDetails.js
+++ my-first-react-app/src/BlogDetails.js
@@ -6,7 +6,7 @@ const BlogData = ({title, author, body}) => {
     return (
         <div>
             <h1>{title}</h1>
-            <h3>{author}</h3>
+            <h3>written by: {author}</h3>
             <p>
                 {body}
             </p>
@@ -15,15 +15,16 @@ const BlogData = ({title, author, body}) => {
 }
 
 const BlogDetails = () => {
+    
     const {id} = useParams()
-
     const {data, pending, error} = useFetch(`http://localhost:5500/blogs/${id}`)
+
     return (
             <div>
-                {pending && <h1>Loading ...</h1>},
                 {error && <h3>could not fetch the data from the the specific resource</h3>}
-            </div>,
-            data && <BlogData title={data.title} author={data.author} body={data.body}/>
+                {pending && <h1>Loading ...</h1>}
+                {data && <BlogData title={data.title} author={data.author} body={data.body}/>}
+            </div>
     );
 }
  
--- my-first-react-app/src/blog.js
+++ my-first-react-app/src/blog.js
@@ -11,10 +11,10 @@ const BlogList = ({data, title})=>
                 {
                     return (
                         <div className="blog-preview" key={ele.id}>
-                        <Link to={`blogs/${ele.id}`}>
-                                <h2>{ele.title}</h2>
-                                <p>written by {ele.author} </p>
-                        </Link>
+                            <Link to={`blogs/${ele.id}`}>
+                                    <h2>{ele.title}</h2>
+                                    <p>written by {ele.author} </p>
+                            </Link>
                         </div>
                     )
                    
