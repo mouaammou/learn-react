b52ae96136a0451472b02288b5d321c2cd2e1f86
mouaammou <mouadamassine@gmail.com>
2024-04-22 15:48:04 +0100

    learn react router 6

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,14 +1,22 @@
 import Navbar from "./navbar";
 import Home from "./home";
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import CreateNews from "./news";
 
 function App() {
     return (
-        <div className="App">
-           <Navbar />
-            <div className="content">
-                <Home />
-            </div> 
-        </div>
+        <Router>
+            <div className="App">
+            <Navbar />
+                <div className="content">
+                    <Routes>
+                        <Route exact path="/" element={<Home />} />
+                        <Route exact path="/news" element={<CreateNews />} />
+                    </Routes>
+                </div> 
+            </div>
+
+        </Router>
     );
 }
 
---
+++ my-first-react-app/src/news.js
@@ -0,0 +1,9 @@
+const CreateNews = () => {
+    return (
+        <div className="new-news">
+            <h1>hello new news about the world</h1>
+        </div>
+    );
+}
+ 
+export default CreateNews;
